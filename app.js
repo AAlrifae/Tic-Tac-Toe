a991a9d3a7896c9c71d0ac9112c12a6fcd605df6
AAlrifae <a.a.alrifae@gmail.com>
2019-07-03 21:04:45 +0300

    add comments

--- app.js
+++ app.js
@@ -2,32 +2,17 @@ console.log('hi');
 
 const boxes = $('td');
 let player = 'X';
-
-console.log(boxes);
 let tie = 0;
 let player1 = 'X';
 let player2 = 'O';
-// const player1Anime = "<svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'><circle id='circle' cx='50' cy='50' r='45'/> </svg>";
 let player1Score = 0;
 let player2Score = 0;
-//========= DOM============
-// const player1Input = document.getElementById('player1Name');
-// const changeName = function() {
-//     const player1Name = document.getElementById('player1');
-//     player1Name.innerText = player1Input.value;   
-// }
-
-// document.querySelector('#player1').addEventListener('click', function(){
-//     console.log(this);
-//     console.log(this.select);
-//     this.select();
-// });
-
-// player1Input.addEventListener('keypress', changeName);
+//check function also switch turn
 const changeOnCLick = function() {
     tie++;
     if (this.innerText == '') {
     this.innerText = player;
+    //add the class move to animate the X,O
     this.classList += ' move';
     switch (player) {
         case player1:
@@ -39,35 +24,37 @@ const changeOnCLick = function() {
             break;
         }  
     }
-    // debugger;
+    // return the value of the winner and put it into variable
     const winner = win();
     if(winner){
-        // debugger;
+        // check if the winner is X
         if (winner == player1) {
             player1Score++;
             document.querySelector('.score1').innerText = player1Score;
-            console.log(player1Score);
-            
-        } else {
+            // console.log(player1Score);  
+        } 
+        //check if the winner is O
+        else {
             player2Score++;
             document.querySelector('.score2').innerText = player2Score;
-            console.log(player2Score)
+            // console.log(player2Score)
         }
     }
 }   
-// The win function
+//==============Dom==================
 const winnerDisplay = document.querySelector('#h1');
 const winnerDiv = document.querySelector('#typewriter');
 const drawDisplay = document.querySelector('#h2')
 const drawDiv = document.querySelector('#typewriter1');
 
+// The win function
 const win = function() {
-    //declare boxes
+    //get the box ID
     const boxesValues= []
     for (let i = 1; i < 10; i++) {
         window['box'+i] = document.querySelector(`#box-${i}`).innerText || `${i}`  
     }
-    //create array 
+    // Array contain the conditions
     const check = [
         ( (box1 == box2) && (box1 == box3) )? box1: '', 
         ( (box4 == box5) && (box4 == box6) )? box4: '', 
@@ -78,36 +65,40 @@ const win = function() {
         ( (box1 == box5) && (box1 == box9) )? box1: '',
         ( (box3 == box5) && (box3 == box7) )? box3: '',
     ]
+    //check if the condition is true
     for (let i = 0; i < check.length; i++) {
         if(check[i]){
-            console.log(check[i])
-             winnerDisplay.classList += ' win'
+            winnerDisplay.classList += ' win'
+            //add class typewriter to animate the winner
             winnerDiv.classList += ' typewriter'
             winnerDisplay.innerText = `winner ${check[i]}`
-            console.log('winner', check[i]);
+           //if winner is true then remove all the clicks
             boxes.unbind( "click", changeOnCLick );
             tie = 0;
-
             return  check[i];
         } 
     }
     if (tie === 9){
+        // if it's draw then remove all the clicks
         boxes.unbind( "click", changeOnCLick );
+        // add class typewriter to animate
         drawDiv.classList += ' typewriter'
         drawDisplay.innerText = `DRAW `
-    console.log('tie');
+    // console.log('tie');
             }
     return false;
     }
-
+    //Reset function 
     const resetGame = function () {
+        //make all the inner text for td empty
         for (let i = 0; i < boxes.length; i++) {
              boxes[i].innerText = '' ;
         }
+        //remove all the animated classes
         for (let i = 1; i < 10; i++) {
             window['box'+i] = document.querySelector(`#box-${i}`).classList.remove('move')  
         }
-        winnerDisplay.innerText = ``;
+         winnerDisplay.innerText = ``;
          winnerDisplay.classList.remove('win');
          winnerDiv.classList.remove('typewriter');
          drawDiv.classList.remove('typewriter')
